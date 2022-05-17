f8a539cf4097db7147881eb1630874f9bf182036
EnragedWaters <49539862+EnragedWaters@users.noreply.github.com>
2022-05-16 22:25:49 -0700

    Uploading Files~
    Description goes here!

---
+++ ElephantsAreAMAZING.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 0; i < 10; i++) {
+		cout << "     __" << endl
+			<< " .--()°'.'" << endl
+			<< "'|, . ,'" << endl
+			<< "!_ - (_\"" << endl;
+	}
+}
